eab80718beec2f846aa7b8f7dc048ea0d55b6909
BillMills <mills.wj@gmail.com>
2013-12-18 13:25:17 -0800

    getting mongo off the ground

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ app.post('/create', function(req, res){
 		});
 	});
 
-/*
+
 	mongo.Db.connect(mongoUri, function(err, db) {
 		db.collection('mydocs', function(er, collection) {
 			collection.find({}, function(err, stuff){
@@ -33,8 +33,8 @@ app.post('/create', function(req, res){
 			});
 		});
 	});
-*/
 
+/*
 	mongo.Db.connect(mongoUri, function(err, db) {
 		db.collection('mydocs', function(er, collection) {
 			collection.find({}, function(err, stuff){
@@ -50,7 +50,7 @@ app.post('/create', function(req, res){
 			});
 		});
 	});
-
+*/
 	res.redirect('/');
 
 
