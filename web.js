ec43a64925349f5e03d3e035a5e6e2c8922df860
BillMills <mills.wj@gmail.com>
2013-12-17 21:30:35 -0800

    getting mongo off the ground

--- web.js
+++ web.js
@@ -4,23 +4,25 @@ var logfmt = require("logfmt");
 var mongo = require('mongodb');
 var app = express();
 
-app.use(logfmt.requestLogger());
-app.use(express.static(__dirname));
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://heroku_app20467917:j5f8u413gre79i0o24km87ut0b@ds059898.mongolab.com:59898/heroku_app20467917';
+
+//app.use(logfmt.requestLogger());
+//app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
   //res.redirect("/index.html");
   res.render('index', {title:'plzWork'});
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
 
 
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://heroku_app20467917:j5f8u413gre79i0o24km87ut0b@ds059898.mongolab.com:59898/heroku_app20467917';
+
 
 mongo.Db.connect(mongoUri, function (err, db) {
   db.collection('mydocs', function(er, collection) {
