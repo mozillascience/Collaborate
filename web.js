38e4ad430f9ad4ac44aabc51137ab4958e86d524
BillMills <mills.wj@gmail.com>
2013-12-24 00:39:41 -0800

    implementing backend structure

--- web.js
+++ web.js
@@ -164,7 +164,7 @@ app.post('/emailNewPassword', function(req, res){
 				        	if(err) return res.render('error.jade');
 
 				        	//update db
-				        	user.update({Pass : hash})
+				        	collection.update({_id : user._id}, {Pass : hash})
 							
 				        });
 			    });
