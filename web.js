bfd7f469d5e10bc6720c48548f6cb720f6cc39eb
BillMills <mills.wj@gmail.com>
2013-12-26 15:49:23 -0800

    implementing user details update page

--- web.js
+++ web.js
@@ -91,7 +91,7 @@ app.get('/setupNewUser', function(req, res){
 
 //user profile page
 app.get('/userProfile', function(req, res){
-
+console.log(req.user)
 	res.render('userProfile.jade', {scientist: req.user.scientist, developer:req.user.developer});
 
 });
