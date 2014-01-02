1d793f645fb553545324f7a2e5dca91d56c3fb8d
BillMills <mills.wj@gmail.com>
2014-01-01 16:54:38 -0800

    email profile

--- web.js
+++ web.js
@@ -287,11 +287,13 @@ app.post('/recordUpdate', function(req, res){
 		    	//update the local user object
 		    	req.user.discipline = req.body.discipline;
 		    	req.user.language = req.body.language;
+		    	req.user.email = req.body.email;
 
 		    	//update the DB and carry on to main user pages
 		    	collection.update(	{uName : user.uName}, 
 		    						{$set:{	discipline : req.body.discipline, 
-		    								language : req.body.language}
+		    								language : req.body.language,
+		    								email : req.body.email}
 		    						}, 
 		    						function(){
 										return res.redirect('/userMatches?page=0');									
