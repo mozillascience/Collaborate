44f1efc0f3d417c7fd0b16efd4b4b2d47502416c
BillMills <mills.wj@gmail.com>
2014-01-15 22:25:47 -0800

    search validation

--- routes.js
+++ routes.js
@@ -344,10 +344,10 @@ app.post('/search', function(req, res){
 
 	mongo.Db.connect(mongoUri, function(err, db) {
 		db.collection('Users', function(er, collection) {	
-			var scientist = (req.body.profession == 'scientist') ? true : false;
+			var scientist = (req.body.profession == 'scientist') ? true : false,
 
 	    	collection.find( {	scientist: scientist, 
-	    						language : {$in: (req.body.language ? languages : req.body.language)}, 
+	    						language : {$in: (req.body.language ? req.body.language : languages)}, 
 	    						discipline : {$in: req.body.discipline}
 	    					} ).toArray(function(err, matches){
 	    							searchBuffer[req.user['_id']] = matches;
