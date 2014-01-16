e90c1f39e925501901bd0234301b9854e16882ce
BillMills <mills.wj@gmail.com>
2014-01-15 19:48:58 -0800

    factoring out routes to route.js

--- options.js
+++ options.js
@@ -1,11 +1,11 @@
 //define all the options for profile definitions here
 
-exports.disciplines = 	[ 	'Divination',
-							'Evokation',
-							'Necromancy'
-						],
+disciplines = 	[ 	'Divination',
+					'Evokation',
+					'Necromancy'
+				],
 
-exports.languages = [ 	'Haskell',
-						'Visual BASIC',
-						'Jade'
-					];
+languages = [ 	'Haskell',
+				'Visual BASIC',
+				'Jade'
+			];
