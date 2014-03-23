76de90f4ff3a07b3ee1d2eeb5d60e1fb150b83c9
BillMills <mills.wj@gmail.com>
2014-03-23 12:25:34 -0700

    fixes #11

--- static/formHandler.js
+++ static/formHandler.js
@@ -1,15 +1,16 @@
 //function for binding to the onclick of the scientist/developer
-//radio in the sign-up form; displays the rest of the form the
-//first time a profession is chosen, and ensures the profession-
+//radio in the sign-up and user profile form; displays the rest of the form the
+//first time a profession is chosen during signup, and ensures the profession-
 //dependent labels for all further questions is correctly inserted.
-function manageSignupForm(){
+function manageUserForm(){
 	var wrapperDiv = document.getElementById('registrationDetails')
 	,	isScientist
 	,	disciplineText = document.getElementById('disciplineText')
 	,	languageText = document.getElementById('languageText');
 
 	//switch the details section of the form to opacity=1 and allow height expansion:
-	wrapperDiv.setAttribute('style', 'max-height: 2000px; opacity: 1');
+	if(wrapperDiv)
+		wrapperDiv.setAttribute('style', 'max-height: 2000px; opacity: 1');
 
 	//determine which radio button got clicked:
 	isScientist = document.getElementById('science').checked
